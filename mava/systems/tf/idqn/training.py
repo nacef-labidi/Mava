c58c4c5e5a852fa9b42fdf889df6899fe3912e2a
Claude Formanek <formanekjc@gmail.com>
2021-09-15 08:04:37 +0200

    Dist Double DQN.

--- mava/systems/tf/idqn/training.py
+++ mava/systems/tf/idqn/training.py
@@ -311,18 +311,22 @@ class IDQNTrainer(mava.Trainer):
                     support = self._network_supports[net_key]
 
                     logits = self._q_networks[net_key](net_observations[net_key])
-
                     target_logits = self._target_q_networks[net_key](net_next_observations[net_key])
 
-                    # trfl distributional Q-learning
-                    loss, _ = trfl.categorical_dist_qlearning(
+                    q_t_selector_logits = self._q_networks[net_key](net_next_observations[net_key])
+                    q_t_selector_z = tf.nn.softmax(q_t_selector_logits, axis=-1)
+                    q_t_selector = tf.reduce_sum(q_t_selector_z * support, axis=-1)  
+
+                    # trfl distributional double Q-learning
+                    loss, _ = trfl.categorical_dist_double_qlearning(
                         support,
                         logits,
                         net_actions[net_key],
                         net_rewards[net_key],
                         self._discount * net_discounts[net_key],
                         support, 
-                        target_logits
+                        target_logits,
+                        q_t_selector
                     )
                 else:
                     q_tm1 = self._q_networks[net_key](net_observations[net_key])
